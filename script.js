d93e0d362238e85ddb46db2a77116ed42fcf2b15
HCHsanjacHamilton <Hamilton.H238666@stu.sanjac.edu>
2026-04-16 16:53:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.getElementById("gallery");
+  const box = document.querySelector(".box");
+  const colorButton = document.getElementById("changeColor");
+  const toggleButton = document.getElementById("toggleImage");
+
+  colorButton.addEventListener("click", () => {
+    box.style.backgroundColor = "blue";
+  });
+
+  let toggled = false;
+  toggleButton.addEventListener("click", () => {
+    if (toggled) {
+      gallery.src = "./assets/image1.jpg"; 
+      toggled = false;
+    } else {
+      gallery.src = "./assets/image-does-not-exist.jpg"; 
+      toggled = true;
+    }
+  });
+
+  console.log("Page loaded!");
+});
 
